4d9048554aeee621c6ad637f4d4bfd72bf47532a
Rugby Beentjes <lesquishy@me.com>
2015-05-13 00:11:14 +1200

    Greeting
    Greetings depending on the time of day

--- AB.js
+++ AB.js
@@ -1,32 +1,30 @@
-/* Alerts */
-
-/* Screen Size */
+/* Onload */
 
 if (screen.width < 960) {
 	alert("Your Screen size is less than 960px in width. All Animations have stopped. For optimal quality go to a bigger screen")
 }
 else {
 	alert("Your Screen Size is optimal")
-}
+};
 
-/* Screen Size End */
+window.addEventListener('load', thing);
 
-/* Alerts End */
+/* Onload */
 
 
 /* Functions */
-thing();
+
 function thing() {
 	var greeting;
 	var time = new Date().getHours();
 	if (time < 10) {
-	    greeting = "Good morning";
-	} 
+		greeting = "Good morning";
+	}
 	else if (time < 20) {
-	    greeting = "Good day";
+		greeting = "Good day";
 	}
 	else {
-	    greeting = "Good Evening";
+		greeting = "Good Evening";
 	}
 	document.getElementById("address").innerHTML = greeting;
 }
