7b72065b2632a84735dc3802007f15da029450a0
Rugby Beentjes <lesquishy@me.com>
2015-05-12 23:25:30 +1200

    Javascript Update
    Unfinished update

---
+++ AB.js
@@ -0,0 +1,34 @@
+/* Alerts */
+
+/* Screen Size */
+
+if (screen.width < 960) {
+	alert("Your Screen size is less than 960px in width. All Animations have stopped. For optimal quality go to a bigger screen")
+}
+else {
+	alert("Your Screen Size is optimal")
+}
+
+/* Screen Size End */
+
+/* Alerts End */
+
+
+/* Functions */
+thing();
+function thing() {
+	var greeting;
+	var time = new Date().getHours();
+	if (time < 10) {
+	    greeting = "Good morning";
+	} 
+	else if (time < 20) {
+	    greeting = "Good day";
+	}
+	else {
+	    greeting = "Good Evening";
+	}
+	document.getElementById("address").innerHTML = greeting;
+}
+
+/* Functions End */
