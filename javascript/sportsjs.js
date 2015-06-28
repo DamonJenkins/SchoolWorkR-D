515c754dce6a340ae9ed67eaf7faff3d1575cbf7
Rugby Beentjes <lesquishy@me.com>
2015-06-28 14:15:14 +1200

    More on mobile detection and attempt to fix Log clipping

--- javascript/sportsjs.js
+++ javascript/sportsjs.js
@@ -1,15 +1,24 @@
 function thing() {
 	isMobile()
+	ifMobile()
 }
 
 function isMobile() {
-	try { 
-		document.createEvent("TouchEvent");
-		return true;
-		alert ("Mobile")
+ try { 
+	document.createEvent("TouchEvent");
+	return true;
+ }
+   catch(e) {
+    return false;
+   }
+}
+
+function ifMobile() {
+	if (isMobile() = true) {
+		console.log("Mobile")
+		alert("Mobile")
+	} else {
+		console.log("")
+		alert("Desktop")
 	}
-  	catch(e) {
-  		alert ("Desktop")
-  		return false;
-  	}
 }
